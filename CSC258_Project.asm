09c263ffd2d78f57440665b5c7e4979e6f2c7463
stingythrower <maoladina@gmail.com>
2026-03-21 13:40:29 -0400

    worked on drawing game rectangle

--- CSC258_Project.asm
+++ CSC258_Project.asm
@@ -1,1 +1,106 @@
-# CSC258 Project
+.data
+ADDR_DSPL:
+    .word 0x10008000
+
+######################## Bitmap Display Configuration ########################
+# - Unit width in pixels: 8
+# - Unit height in pixels: 8
+# - Display width in pixels: 256
+# - Display height in pixels: 256
+# - Base Address for Display: 0x10008000 ($gp)
+##############################################################################
+
+.text
+lw $t0, ADDR_DSPL           # $t0 = base address for display
+li $s0, 0xff0000            # $s0 = red
+li $s1, 0x00ff00            # $s1 = green
+li $s2, 0x0000ff            # $s2 = blue
+add $s3, $s0, $s1           # $s3 = yellow
+li $s4, 0xf28c28            # $s4 = cyan
+add $s5,$s0, $s2            # $s5 = magenta
+add $s6, $s3, $s2           # $s6 = white
+
+# initialize the drawing of white game area rectangle
+addi $a0, $zero, 1          # set the X coordinate
+addi $a1, $zero, 1          # set the Y coordinate
+addi $a2, $zero, 8          # set the width of the rectangle (6 + 2)
+addi $a3, $zero, 16         # set the height of the rectangle (14 + 2)
+add $t9, $s6, $zero         # set the colour of the rectangle to be white 
+jal rect_draw               # calls rectangle drawing function.
+
+# initialize the drawing of white game area rectangle
+addi $a0, $zero, 2          # set the X coordinate
+addi $a1, $zero, 2          # set the Y coordinate
+addi $a2, $zero, 6          # set the width of the rectangle (6)
+addi $a3, $zero, 14         # set the height of the rectangle (14)
+add $t9, $zero, $zero       # set the colour of the rectangle to be black
+jal rect_draw               # calls rectangle drawing function.
+
+li $v0, 10                  # terminate the program gracefully (number correspods to the type of syscall)
+syscall
+
+##############################################################################
+# Code for drawing a rectangle
+##############################################################################
+# $t0 = location of the top-left corner of the bitmap
+# $a0 = the X coordinate of the top left corner of the rectangle
+# $a1 = the Y coordinate of the top left corner of the rectangle
+# $a2 = the width of the rectangle
+# $a3 = the height of the rectangle
+# $t1 = the current index in the rectangle drawing loop
+
+rect_draw:
+add $t1, $zero, $zero       # Set a variable to count the current row in the rectangle
+# start the rectangle line drawing code
+rect_loop:
+beq $t1, $a3, rect_loop_end # If we have drawn all the lines for this rectangel, then jump out of the loop
+addi $sp, $sp, -4           # move to an empty spot on the stack (decrement the stack pointer $sp by 4)
+sw $t1, 0($sp)              # push $t1 onto the stack
+
+addi $sp, $sp, -4           # move to an empty spot on the stack (decrement the stack pointer $sp by 4)
+sw $ra, 0($sp)              # push $ra to the stack for nested function
+
+jal line_draw               # Call the line drawing code
+
+lw $ra, 0($sp)              # pop $t1 off the stack
+addi $sp, $sp, 4            # move stack pointer back to the top of the stack
+
+lw $t1, 0($sp)              # pop $t1 off the stack
+addi $sp, $sp, 4            # move stack pointer back to the top of the stack
+
+addi $t1, $t1, 1            # Increment the variable for the current row we are drawing
+addi $a1, $a1, 1            # Increment the Y value to draw on the next line in the bit map
+j rect_loop                 # Jump to the start of the loop
+rect_loop_end:
+jr $ra                      # Return to the calling program
+
+##############################################################################
+# Code for drawing a horizontal line
+##############################################################################
+# $t0 = location of the top-left corner of the bitmap
+# $a0 = the X coordinate of the start of the line
+# $a1 = the Y coordinate of the start of the line
+# $a2 = the length of the line
+# $t1 = the horizontal offset to add to $t0
+# $t2 = the vertical offset to add to $t0
+# $t3 = the current location of the pixel to draw
+# $t4 = the location of the last pixel in the line
+# $t9 = the colour of the line
+
+line_draw: 
+sll $t2, $a1, 7             # Calculate the vertical offset (from $t0), based on the Y input ($a1) (multiply Y input by 128)
+sll $t1, $a0, 2             # Calculate the horizontal offset (from $t0), based on the X intput ($a0) (multiply X input by 4)
+
+add $t3, $t0, $t2           # Add the vertical offset to $t0
+add $t3, $t3, $t1           # Add the horizontal offset to the location calcuated above
+
+sll $t5, $a2, 2             # Calculate the offset from $t3 for the last pixel in the line (multiple $a2 by 4)
+add $t4, $t3, $t5           # Calculate the postion of the last pixel in the line
+# Start of the line-drawing loop
+line_loop:
+beq $t3, $t4, line_loop_end # Check if the current X and Y match the end location of the line, branch out of the loop
+sw $t9, 0($t3)              # Draw a single pixel at the current X and Y
+addi $t3, $t3, 4            # Move to the next pixel in the row
+j line_loop                 # Jump to the start of the loop
+line_loop_end:
+jr $ra                      # return statement (return to where you came from)
