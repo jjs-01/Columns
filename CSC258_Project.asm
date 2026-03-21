587f8813e8d94c60b35fb5b17a357a8acc54b0d4
stingythrower <maoladina@gmail.com>
2026-03-21 15:35:17 -0400

    one randomly generated pixel color

--- CSC258_Project.asm
+++ CSC258_Project.asm
@@ -1,6 +1,6 @@
 .data
-ADDR_DSPL:
-    .word 0x10008000
+ADDR_DSPL: .word 0x10008000
+colors: .word 0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xf28c28, 0xff00ff, 0xffffff   # red, green, blue, yellow, orange, magenta, white
 
 ######################## Bitmap Display Configuration ########################
 # - Unit width in pixels: 8
@@ -9,23 +9,16 @@ ADDR_DSPL:
 # - Display height in pixels: 256
 # - Base Address for Display: 0x10008000 ($gp)
 ##############################################################################
-
 .text
-lw $t0, ADDR_DSPL           # $t0 = base address for display
-li $s0, 0xff0000            # $s0 = red
-li $s1, 0x00ff00            # $s1 = green
-li $s2, 0x0000ff            # $s2 = blue
-add $s3, $s0, $s1           # $s3 = yellow
-li $s4, 0xf28c28            # $s4 = cyan
-add $s5,$s0, $s2            # $s5 = magenta
-add $s6, $s3, $s2           # $s6 = white
+lw $s0, ADDR_DSPL       # $s0 = base address for display
+la $s1, colors          # $s1 = address for the first color
 
 # initialize the drawing of white game area rectangle
 addi $a0, $zero, 1          # set the X coordinate
 addi $a1, $zero, 1          # set the Y coordinate
 addi $a2, $zero, 8          # set the width of the rectangle (6 + 2)
 addi $a3, $zero, 16         # set the height of the rectangle (14 + 2)
-add $t9, $s6, $zero         # set the colour of the rectangle to be white 
+lw $t9, 24($s1)             # loads white from colors
 jal rect_draw               # calls rectangle drawing function.
 
 # initialize the drawing of white game area rectangle
@@ -36,13 +29,48 @@ addi $a3, $zero, 14         # set the height of the rectangle (14)
 add $t9, $zero, $zero       # set the colour of the rectangle to be black
 jal rect_draw               # calls rectangle drawing function.
 
+jal rand_column
 li $v0, 10                  # terminate the program gracefully (number correspods to the type of syscall)
 syscall
 
 ##############################################################################
+# Code for random color column
+##############################################################################
+# $s0 = location of the top-left corner of the bitmap
+# $s1 = address of the first color
+# $t2 = the current location to draw the random color pixel
+
+rand_column:
+addi $sp, $sp, -4           # move to an empty spot on the stack (decrement the stack pointer $sp by 4)
+sw $ra, 0($sp)              # push $ra to the stack for nested function
+
+add $t2, $s0, 272           # add the horizontal - column 5 (4 x 4) and vertical - row 3 (128 x 2) offest to $s0
+
+# randomly generate an integer for one of the six colors
+li $v0, 42
+li $a0, 0
+li $a1, 6                   # 0 to 6 exclusive
+syscall                     # random integer stored in $a0
+
+sll $t3, $a0, 2             # multiply index by 4 (word size)
+add $t3, $t3, $s1           # address = address of first color + offset
+lw $t4, 0($t3)              # load color into $t4
+sw $t4, 0($t2)              # draw pixel of color $t4 to location $t2
+
+lw $ra, 0($sp)              # pop $ra off the stack
+addi $sp, $sp, 4            # move stack pointer back to the top of the stack
+jr $ra
+# draw the pixel
+# move to the next row
+# randomly generate an integer for one of the six colors
+# draw the pixel
+# move to the next row
+# randomly generate an integer for one of the six colors
+# draw the pixel
+##############################################################################
 # Code for drawing a rectangle
 ##############################################################################
-# $t0 = location of the top-left corner of the bitmap
+# $s0 = location of the top-left corner of the bitmap
 # $a0 = the X coordinate of the top left corner of the rectangle
 # $a1 = the Y coordinate of the top left corner of the rectangle
 # $a2 = the width of the rectangle
@@ -53,7 +81,7 @@ rect_draw:
 add $t1, $zero, $zero       # Set a variable to count the current row in the rectangle
 # start the rectangle line drawing code
 rect_loop:
-beq $t1, $a3, rect_loop_end # If we have drawn all the lines for this rectangel, then jump out of the loop
+beq $t1, $a3, rect_loop_end # If we have drawn all the lines for this rectangle, then jump out of the loop
 addi $sp, $sp, -4           # move to an empty spot on the stack (decrement the stack pointer $sp by 4)
 sw $t1, 0($sp)              # push $t1 onto the stack
 
@@ -62,7 +90,7 @@ sw $ra, 0($sp)              # push $ra to the stack for nested function
 
 jal line_draw               # Call the line drawing code
 
-lw $ra, 0($sp)              # pop $t1 off the stack
+lw $ra, 0($sp)              # pop $ra off the stack
 addi $sp, $sp, 4            # move stack pointer back to the top of the stack
 
 lw $t1, 0($sp)              # pop $t1 off the stack
@@ -77,21 +105,21 @@ jr $ra                      # Return to the calling program
 ##############################################################################
 # Code for drawing a horizontal line
 ##############################################################################
-# $t0 = location of the top-left corner of the bitmap
+# $s0 = location of the top-left corner of the bitmap
 # $a0 = the X coordinate of the start of the line
 # $a1 = the Y coordinate of the start of the line
 # $a2 = the length of the line
-# $t1 = the horizontal offset to add to $t0
-# $t2 = the vertical offset to add to $t0
+# $t1 = the horizontal offset to add to $s0
+# $t2 = the vertical offset to add to $s0
 # $t3 = the current location of the pixel to draw
 # $t4 = the location of the last pixel in the line
 # $t9 = the colour of the line
 
 line_draw: 
-sll $t2, $a1, 7             # Calculate the vertical offset (from $t0), based on the Y input ($a1) (multiply Y input by 128)
-sll $t1, $a0, 2             # Calculate the horizontal offset (from $t0), based on the X intput ($a0) (multiply X input by 4)
+sll $t2, $a1, 7             # Calculate the vertical offset (from $s0), based on the Y input ($a1) (multiply Y input by 128)
+sll $t1, $a0, 2             # Calculate the horizontal offset (from $s0), based on the X intput ($a0) (multiply X input by 4)
 
-add $t3, $t0, $t2           # Add the vertical offset to $t0
+add $t3, $s0, $t2           # Add the vertical offset to $s0
 add $t3, $t3, $t1           # Add the horizontal offset to the location calcuated above
 
 sll $t5, $a2, 2             # Calculate the offset from $t3 for the last pixel in the line (multiple $a2 by 4)
